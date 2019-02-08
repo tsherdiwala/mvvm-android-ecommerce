701065c229b9499200028fe1e02fbbb0daa433e6
tsherdiwala <tsherdiwala@gmail.com>
2019-02-08 14:14:52 +0530

    final commit

--- app/src/main/java/com/ezmall/ui/commons/AppBindingAdapter.kt
+++ app/src/main/java/com/ezmall/ui/commons/AppBindingAdapter.kt
@@ -33,39 +33,8 @@ object AppBindingAdapter {
     @JvmStatic
     fun setProductCouponDiscount(view: TextView, text: Float?) {
         val parentActivity = view.getParentActivity()
-        val roundValue = Math.round(text!!)
-        view.text = parentActivity?.getString(R.string.rs_minus, roundValue.toString())
+        view.text = parentActivity?.getString(R.string.rs_minus, text)
     }
-
-    /*@BindingAdapter("mutableTextStatus")
-    fun setMutableTextStatus(view: TextView, text: OrderStatus?) {
-
-        val parentActivity = view.getParentActivity()
-        if (parentActivity != null && text != null) {
-            when {
-                text.orderStatus.name == OrderStatusEnum.ORDERED.name -> {
-                    view.text = parentActivity.getString(R.string.order_Status_ordered)
-                    view.setTextColor(ContextCompat.getColor(parentActivity, android.R.color.holo_red_dark))
-                }
-                text.orderStatus.name == OrderStatusEnum.READY_TO_SHIP.name -> {
-                    view.text = parentActivity.getString(R.string.order_Status_Ready_to_ship)
-                    view.setTextColor(ContextCompat.getColor(parentActivity, android.R.color.holo_blue_dark))
-                }
-                text.orderStatus.name == OrderStatusEnum.CANCELLED.name -> {
-                    view.text = parentActivity.getString(R.string.order_Status_Cancle)
-                    view.setTextColor(ContextCompat.getColor(parentActivity, android.R.color.holo_green_dark))
-                }
-                text.orderStatus.name == OrderStatusEnum.SHIPPED.name -> {
-                    view.text = parentActivity.getString(R.string.order_Status_Shipped)
-                    view.setTextColor(ContextCompat.getColor(parentActivity, android.R.color.holo_orange_light))
-                }
-                text.orderStatus.name == OrderStatusEnum.DELIVERED.name -> {
-                    view.text = parentActivity.getString(R.string.order_Status_Delivered)
-                    view.setTextColor(ContextCompat.getColor(parentActivity, android.R.color.holo_purple))
-                }
-            }
-        }
-    }*/
 
 
 }
